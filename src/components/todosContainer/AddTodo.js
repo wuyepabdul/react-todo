3d6cd7131458ae628e2e7ab1d82931f44b3e8e76
wuyepabdul <wuyepabdul@gmail.com>
2021-10-25 15:13:26 +0100

    Add components directory with children directorys

---
+++ src/components/todosContainer/AddTodo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AddTodo = () => (
+  <div>
+    Add todo
+  </div>
+);
+
+export default AddTodo;
---
+++ src/components/todosContainer/ListTodos.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ListTodos = () => (
+  <div>
+    List todos
+  </div>
+);
+
+export default ListTodos;
